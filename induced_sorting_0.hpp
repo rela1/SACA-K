f0b6332ed894ec05647b2ef00bf3a8346eacc7f7
rela1 <ivan.relic1@gmail.com>
2016-11-01 17:48:52 +0100

    SA type char -> int

--- induced_sorting_0.hpp
+++ induced_sorting_0.hpp
@@ -1,10 +1,10 @@
 #ifndef INDUCED_SORTING_0_H
 #def INDUCED_SORTING_0_H
 
-void induced_sort_LMS_0(char * T, char * SA, int * bkt, int n);
+void induced_sort_LMS_0(char * T, int * SA, int * bkt, int n);
 
-void induced_sort_S_0(char * T, char * SA, int * bkt, int n);
+void induced_sort_S_0(char * T, int * SA, int * bkt, int n);
 
-void induced_sort_L_0(char * T, char * SA, int * bkt, int n);
+void induced_sort_L_0(char * T, int * SA, int * bkt, int n);
 
 #endif
